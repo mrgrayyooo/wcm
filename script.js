81480d4cc56a5ffce26f160b4292e2e022a56015
mrgrayyooo <jf9kzggkxv@privaterelay.appleid.com>
2026-03-13 00:55:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("WAVECLUBMEDIA online")
+console.log("WAVECLUBMEDIA V2 loaded");
+
+// Glow animation для кнопок
+const buttons = document.querySelectorAll('.btn');
+buttons.forEach(btn=>{
+  btn.addEventListener('mouseenter',()=>btn.style.boxShadow='0 0 25px #CC0000');
+  btn.addEventListener('mouseleave',()=>btn.style.boxShadow='0 0 0 #000');
+});
